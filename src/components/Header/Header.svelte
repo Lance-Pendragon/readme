dce00f113fdcb7ccd706a4940aea614ab61e9abd
lancejabuan@gmail.com <lancejabuan@gmail.com>
2025-03-17 17:47:25 -0500

    feature/17-convert-to-svelte : Converting Header to svelte

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+  import NavBarStart from "./NavBarStart.svelte";
+  import NavBarCenter from "./NavBarCenter.svelte";
+  import NavBarEnd from "./NavBarEnd.svelte";
+</script>
+
+<div className="navbar bg-base-100 shadow-sm">
+  <NavBarStart />
+  <NavBarCenter />
+  <NavBarEnd />
+</div>
