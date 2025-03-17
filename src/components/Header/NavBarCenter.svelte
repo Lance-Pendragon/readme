4a2bfee921241fd05cd30acae27d8a066fd9fa1c
lancejabuan@gmail.com <lancejabuan@gmail.com>
2025-03-17 17:45:42 -0500

    feature/17-convert-to-svelte : Converting NavBarCenter to svelte

---
+++ src/components/Header/NavBarCenter.svelte
@@ -0,0 +1,3 @@
+<div className="navbar-center">
+  <a className="btn btn-ghost text-xl">Lance Pendragon</a>
+</div>
--- src/components/Header/NavBarCenter.tsx
+++
@@ -1,7 +0,0 @@
-export default function NavBarCenter() {
-    return (
-        <div className="navbar-center">
-            <a className="btn btn-ghost text-xl">Lance Pendragon</a>
-        </div>
-    )
-}
