c2eab30bd37306af5f84cdf6760dc89f60f2780c
lancejabuan@gmail.com <lancejabuan@gmail.com>
2025-03-17 17:42:14 -0500

    feature/17-convert-to-svelte : Converting MainPageHero to svelte

---
+++ src/components/MainPageHero/MainPageHero.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Greetings from "./Greetings.svelte";
+</script>
+
+<div
+  className="hero min-h-screen"
+  style={{
+    backgroundImage:
+      "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}
+>
+  <div className="hero-overlay"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <Greetings />
+  </div>
+</div>
