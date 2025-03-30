785c002c323766d8b6b5278a4fc571de228eaa91
Lance Pendragon <lancejabuan@gmail.com>
2025-03-29 23:17:21 -0500

    feature/12-blog-listing-page : Blog landing has been done

--- src/components/BlogLanding/BlogLanding.svelte
+++ src/components/BlogLanding/BlogLanding.svelte
@@ -1,1 +1,15 @@
-asdf
+<script>
+
+</script>
+
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content">
+    <div class="w-xl">
+      <h1 class="text-5xl font-bold">While (True):</h1>
+      <p class="py-6">
+        Keeping you in the loop!
+      </p>
+      <div class="divider divider-primary">Recent Posts</div>
+    </div>
+  </div>
+</div>
